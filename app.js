c0b9b856e2cd754bf4f13116f339a96cf5899c13
STIVVVV <stevencanfield2000@gmail.com>
2022-04-29 17:21:09 -0400

    Added the rest of the functionality.

--- app.js
+++ app.js
@@ -53,7 +53,10 @@ const cardArray =
 cardArray.sort(() => 0.5 - Math.random())
 
 const gridDisplay = document.querySelector('#grid')
-const cardsChosen = []
+const resultDisplay = document.querySelector('#result')
+let cardsChosen = []
+let cardsChosenIds = []
+const cardsWon = []
 
 function createBoard()
 {
@@ -68,16 +71,53 @@ function createBoard()
 }
 
 createBoard()
+
+function checkMatch()
 {
+    const cards = document.querySelectorAll('img')
+    const optionOneId = cardsChosenIds[0]
+    const optionTwoId = cardsChosenIds[1]
+    console.log(cards)
     console.log('check for match')
+    if(optionOneId == optionTwoId)
+    {
+        cards[optionOneId].setAttribute('src', 'images/blank.png')
+        cards[optionTwoId].setAttribute('src', 'images/blank.png')
+        alert('You have clicked the same image!')
+    }
+
+    if(cardsChosen[0] == cardsChosen[1])
+    {
+        alert('You found a match!')
+        cards[optionOneId].setAttribute('src', 'images/white.png')
+        cards[optionTwoId].setAttribute('src', 'images/white.png')
+        cards[optionOneId].removeEventListener('click', flipCard)
+        cards[optionTwoId].removeEventListener('click', flipCard)
+        cardsWon.push(cardsChosen)
+    }
+    else
+    {
+        cards[optionOneId].setAttribute('src', 'images/blank.png')
+        cards[optionTwoId].setAttribute('src', 'images/blank.png')
+        alert('Sprry try again!')
+    }
+    resultDisplay.textContent = cardsWon.length
+    cardsChosen = []
+    cardsChosenIds = []
+
+    if(cardsWon.length == cardArray.length/2)
+    {
+        resultDisplay.innerHTML = 'Congradulations you found them all!'
+    }
 }
-
-
 
 function flipCard()
 {
     const cardId = this.getAttribute('data-id')
     cardsChosen.push(cardArray[cardId].name)
+    cardsChosenIds.push(cardId)
+    console.log(cardsChosen)
+    console.log(cardsChosenIds)
     this.setAttribute('src', cardArray[cardId].img)
     if(cardsChosen.length === 2)
     {
